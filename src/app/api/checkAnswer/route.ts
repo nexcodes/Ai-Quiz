ba80dc314f527b081300882173b74d87f3caa7d6
Mian Mohid Naeem <113250876+nexcodes@users.noreply.github.com>
2024-03-08 02:19:28 +0500

    updates

--- src/app/api/checkAnswer/route.ts
+++ src/app/api/checkAnswer/route.ts
@@ -6,10 +6,13 @@ import { checkAnswerType } from "@/types/questions";
 export async function POST(req: Request, res: Response) {
   try {
     const body = await req.json();
+
     const { questionId, userInput } = checkAnswerType.parse(body);
+
     const question = await prisma.question.findUnique({
       where: { id: questionId },
     });
+
     if (!question) {
       return NextResponse.json(
         {
@@ -20,34 +23,50 @@ export async function POST(req: Request, res: Response) {
         }
       );
     }
+
     await prisma.question.update({
       where: { id: questionId },
       data: { userAnswer: userInput },
     });
+
     if (question.questionType === "mcq") {
+
       const isCorrect =
         question.answer.toLowerCase().trim() === userInput.toLowerCase().trim();
+
       await prisma.question.update({
         where: { id: questionId },
         data: { isCorrect },
       });
+
       return NextResponse.json({
         isCorrect,
       });
+
     } else if (question.questionType === "open_ended") {
+
       let percentageSimilar = stringSimilarity.compareTwoStrings(
         question.answer.toLowerCase().trim(),
         userInput.toLowerCase().trim()
       );
+
       percentageSimilar = Math.round(percentageSimilar * 100);
+
       await prisma.question.update({
         where: { id: questionId },
         data: { percentageCorrect: percentageSimilar },
       });
+
       return NextResponse.json({
         percentageSimilar,
       });
+
     }
+
+    return NextResponse.json({
+      message: "Question type not found",
+    });
+    
   } catch (error) {
     if (error instanceof ZodError) {
       return NextResponse.json(
--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -15,7 +15,7 @@ const History = async (props: Props) => {
     return redirect("/");
   }
   return (
-    <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-[400px]">
+    <div className="">
       <Card>
         <CardHeader>
           <div className="flex items-center justify-between">
@@ -26,7 +26,7 @@ const History = async (props: Props) => {
             </Link>
           </div>
         </CardHeader>
-        <CardContent className="max-h-[60vh] overflow-scroll">
+        <CardContent className="">
           <HistoryComponent limit={100} userId={session.user.id} />
         </CardContent>
       </Card>
